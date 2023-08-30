9048af10acdbdf36ddfe259dfc81e6984cbf0250
bla113 <32277733+bla113@users.noreply.github.com>
2023-08-30 08:40:14 -0600

    Cambio vista de materias
    Cambio de vista materia

--- controladores/materias.controlador.php
+++ controladores/materias.controlador.php
@@ -5,7 +5,7 @@ class ControladorMaterias
     static public function ctrMostrarMateria($item, $valor)
     {
 
-        $tabla = "materias";
+        $tabla = "vista_materias_grupos";
 
         $respuesta = ModeloMateria::mdlMostrarMateria($tabla, $item, $valor);
 
